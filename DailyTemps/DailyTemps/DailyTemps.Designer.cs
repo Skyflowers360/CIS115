9d8839ab25cd6a34919c3def983468da2450cb2c
Darcy <dsmit7021@students.southwesterncc.edu>
2025-03-30 21:15:01 -0400

    Add files via upload

---
+++ DailyTemps/DailyTemps/DailyTemps.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace DailyTemps
+{
+	partial class DailyTemps
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DailyTemps));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.buttonsubmit = new System.Windows.Forms.Button();
+            this.labelresult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(48, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(48, 112);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(48, 173);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(48, 228);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(48, 289);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(48, 349);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 5;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(48, 406);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 6;
+            // 
+            // buttonsubmit
+            // 
+            this.buttonsubmit.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonsubmit.Location = new System.Drawing.Point(299, 43);
+            this.buttonsubmit.Name = "buttonsubmit";
+            this.buttonsubmit.Size = new System.Drawing.Size(151, 40);
+            this.buttonsubmit.TabIndex = 7;
+            this.buttonsubmit.Text = "Enter";
+            this.buttonsubmit.UseVisualStyleBackColor = true;
+            this.buttonsubmit.Click += new System.EventHandler(this.buttonsubmit_Click);
+            // 
+            // labelresult
+            // 
+            this.labelresult.AutoSize = true;
+            this.labelresult.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelresult.Location = new System.Drawing.Point(208, 173);
+            this.labelresult.Name = "labelresult";
+            this.labelresult.Size = new System.Drawing.Size(0, 18);
+            this.labelresult.TabIndex = 8;
+            this.labelresult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // DailyTemps
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(735, 544);
+            this.Controls.Add(this.labelresult);
+            this.Controls.Add(this.buttonsubmit);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "DailyTemps";
+            this.Text = "DailyTemps";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.Button buttonsubmit;
+		private System.Windows.Forms.Label labelresult;
+	}
+}
+
---
+++ DailyTemps/DailyTemps/DailyTemps.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DailyTemps
+{
+    public partial class DailyTemps : Form
+    {
+        private int[] temperatures = new int[7];
+        private int totalTemperature = 0;
+
+        public DailyTemps()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonsubmit_Click(object sender, EventArgs e)
+        {
+            TextBox[] tempTextBoxes = { textBox1, textBox2, textBox3, textBox4, textBox5, textBox6, textBox7 };
+            bool allValid = true;
+            totalTemperature = 0;
+
+            for (int i = 0; i < tempTextBoxes.Length; i++)
+            {
+                int enteredTemperature;
+                if (int.TryParse(tempTextBoxes[i].Text, out enteredTemperature) && enteredTemperature >= -20 && enteredTemperature <= 130)
+                {
+                    temperatures[i] = enteredTemperature;
+                    totalTemperature += enteredTemperature;
+                }
+                else
+                {
+                    allValid = false;
+                    break;
+                }
+            }
+
+            if (allValid)
+            {
+                double averageTemperature = totalTemperature / 7.0;
+                labelresult.Text = "Temperatures: " + string.Join(", ", temperatures) +
+                                 "\nAverage Temperature: " + averageTemperature.ToString("F2");
+
+                buttonsubmit.Enabled = true;
+
+                foreach (var textBox in tempTextBoxes)
+                {
+                    textBox.Clear();
+                }
+            }
+            else
+            {
+                labelresult.Text = "Please enter valid temperatures between -20 and 130°F.";
+            }
+        }
+    }
+}

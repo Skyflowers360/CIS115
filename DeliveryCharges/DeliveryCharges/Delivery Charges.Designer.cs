fc73f02806452517802aeb32a0b678e54f0a4180
Darcy <dsmit7021@students.southwesterncc.edu>
2025-04-06 14:43:12 -0400

    Add files via upload

---
+++ DeliveryCharges/DeliveryCharges/Delivery Charges.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace DeliveryCharges
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonCheckDelivery = new System.Windows.Forms.Button();
+            this.textZipCode = new System.Windows.Forms.TextBox();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(434, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter a zip code to check delivery charges";
+            // 
+            // buttonCheckDelivery
+            // 
+            this.buttonCheckDelivery.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCheckDelivery.Location = new System.Drawing.Point(210, 97);
+            this.buttonCheckDelivery.Name = "buttonCheckDelivery";
+            this.buttonCheckDelivery.Size = new System.Drawing.Size(263, 37);
+            this.buttonCheckDelivery.TabIndex = 1;
+            this.buttonCheckDelivery.Text = "Check Delivery Charge";
+            this.buttonCheckDelivery.UseVisualStyleBackColor = true;
+            this.buttonCheckDelivery.Click += new System.EventHandler(this.buttonCheckDelivery_Click);
+            // 
+            // textZipCode
+            // 
+            this.textZipCode.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textZipCode.Location = new System.Drawing.Point(35, 99);
+            this.textZipCode.Name = "textZipCode";
+            this.textZipCode.Size = new System.Drawing.Size(133, 32);
+            this.textZipCode.TabIndex = 2;
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResult.Location = new System.Drawing.Point(71, 275);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 21);
+            this.labelResult.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(519, 518);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.textZipCode);
+            this.Controls.Add(this.buttonCheckDelivery);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonCheckDelivery;
+        private System.Windows.Forms.TextBox textZipCode;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ DeliveryCharges/DeliveryCharges/Delivery Charges.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeliveryCharges
+{
+    public partial class Form1 : Form
+    {
+        int[] zipCodes = { 27510, 27502, 27511, 28358, 28625, 28779, 28801, 27401, 28205, 27601 };
+        decimal[] deliveryCharges = { 15.00m, 20.00m, 25.00m, 18.50m, 22.00m, 19.75m, 21.00m, 23.50m, 17.00m, 24.00m };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCheckDelivery_Click(object sender, EventArgs e)
+        {
+            string userInput = textZipCode.Text;
+            bool found = false;
+
+            if (int.TryParse(userInput, out int enteredZip))
+            {
+                for (int i = 0; i < zipCodes.Length; i++)
+                {
+                    if (zipCodes[i] == enteredZip)
+                    {
+                        labelResult.Text = $"The delivery charge to zip code {enteredZip} is ${deliveryCharges[i]:0.00}.";
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found)
+                {
+                    labelResult.Text = $"Sorry, we do not deliver to zip code {enteredZip}.";
+                }
+            }
+            else
+            {
+                labelResult.Text = "Please enter a valid zip code.";
+            }
+        }
+    }
+}

bf20157abbb84e0e31d05bcb17b7ac68880240df
Darcy <dsmit7021@students.southwesterncc.edu>
2025-04-20 15:35:58 -0400

    Add files via upload

---
+++ FineForOverdueBooks/FineForOverdueBooks/FineForOverdueBooks.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace FineForOverdueBooks
+{
+    partial class FineForOverdueBooks
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FineForOverdueBooks));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBooks = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textDays = new System.Windows.Forms.TextBox();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.lableResults = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(264, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of books checked out";
+            // 
+            // textBooks
+            // 
+            this.textBooks.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBooks.Location = new System.Drawing.Point(489, 136);
+            this.textBooks.Name = "textBooks";
+            this.textBooks.Size = new System.Drawing.Size(100, 29);
+            this.textBooks.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(33, 276);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(218, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Number of days overdue";
+            // 
+            // textDays
+            // 
+            this.textDays.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textDays.Location = new System.Drawing.Point(488, 276);
+            this.textDays.Name = "textDays";
+            this.textDays.Size = new System.Drawing.Size(100, 29);
+            this.textDays.TabIndex = 3;
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCalculate.Location = new System.Drawing.Point(37, 405);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(244, 35);
+            this.buttonCalculate.TabIndex = 4;
+            this.buttonCalculate.Text = "Calculate Fine";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // lableResults
+            // 
+            this.lableResults.AutoSize = true;
+            this.lableResults.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lableResults.Location = new System.Drawing.Point(445, 407);
+            this.lableResults.Name = "lableResults";
+            this.lableResults.Size = new System.Drawing.Size(0, 25);
+            this.lableResults.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(165, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(289, 29);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Fines For Overdue Books";
+            // 
+            // FineForOverdueBooks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(638, 643);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lableResults);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.textDays);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBooks);
+            this.Controls.Add(this.label1);
+            this.Name = "FineForOverdueBooks";
+            this.Text = "FineForOverdueBooks";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBooks;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textDays;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.Label lableResults;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ FineForOverdueBooks/FineForOverdueBooks/FineForOverdueBooks.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FineForOverdueBooks
+{
+    public partial class FineForOverdueBooks : Form
+    {
+        public FineForOverdueBooks()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBooks.Text, out int books) && int.TryParse(textDays.Text, out int days))
+            {
+                double fine = CalculateFine(books, days);
+                lableResults.Text = $"Total fine: ${fine:F2}";
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid numbers for books and days.");
+            }
+        }
+
+        private double CalculateFine(int books, int days)
+        {
+            const double rateFirst7Days = 0.10;
+            const double rateAfter7Days = 0.20;
+
+            int daysAtRate1 = Math.Min(days, 7);
+            int daysAtRate2 = Math.Max(days - 7, 0);
+
+            double fine = (books * daysAtRate1 * rateFirst7Days) + (books * daysAtRate2 * rateAfter7Days);
+            return fine;
+        }
+    }
+}

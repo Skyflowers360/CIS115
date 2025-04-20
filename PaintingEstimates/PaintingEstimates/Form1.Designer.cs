d6dcb3a6defd78b5f092eabb7c936f6d91e9e02e
Darcy <dsmit7021@students.southwesterncc.edu>
2025-04-20 16:41:10 -0400

    Add files via upload

---
+++ PaintingEstimates/PaintingEstimates/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace PaintingEstimates
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelLength = new System.Windows.Forms.Label();
+            this.textLength = new System.Windows.Forms.TextBox();
+            this.labelWidth = new System.Windows.Forms.Label();
+            this.textWidth = new System.Windows.Forms.TextBox();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Perpetua", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.label1.Location = new System.Drawing.Point(87, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(593, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "How much it will cost to paint a room.";
+            // 
+            // labelLength
+            // 
+            this.labelLength.AutoSize = true;
+            this.labelLength.BackColor = System.Drawing.Color.Transparent;
+            this.labelLength.Font = new System.Drawing.Font("Perpetua", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLength.Location = new System.Drawing.Point(45, 134);
+            this.labelLength.Name = "labelLength";
+            this.labelLength.Size = new System.Drawing.Size(128, 28);
+            this.labelLength.TabIndex = 1;
+            this.labelLength.Text = "Length (ft):";
+            // 
+            // textLength
+            // 
+            this.textLength.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.textLength.Font = new System.Drawing.Font("Perpetua", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textLength.Location = new System.Drawing.Point(188, 128);
+            this.textLength.Name = "textLength";
+            this.textLength.Size = new System.Drawing.Size(100, 35);
+            this.textLength.TabIndex = 2;
+            // 
+            // labelWidth
+            // 
+            this.labelWidth.AutoSize = true;
+            this.labelWidth.BackColor = System.Drawing.Color.Transparent;
+            this.labelWidth.Font = new System.Drawing.Font("Perpetua", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWidth.Location = new System.Drawing.Point(48, 204);
+            this.labelWidth.Name = "labelWidth";
+            this.labelWidth.Size = new System.Drawing.Size(122, 28);
+            this.labelWidth.TabIndex = 3;
+            this.labelWidth.Text = "Width (ft):";
+            // 
+            // textWidth
+            // 
+            this.textWidth.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.textWidth.Font = new System.Drawing.Font("Perpetua", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textWidth.Location = new System.Drawing.Point(188, 204);
+            this.textWidth.Name = "textWidth";
+            this.textWidth.Size = new System.Drawing.Size(100, 35);
+            this.textWidth.TabIndex = 4;
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.buttonCalculate.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonCalculate.Font = new System.Drawing.Font("Perpetua", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCalculate.Location = new System.Drawing.Point(51, 300);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(125, 72);
+            this.buttonCalculate.TabIndex = 5;
+            this.buttonCalculate.Text = "Calculate Cost";
+            this.buttonCalculate.UseVisualStyleBackColor = false;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.BackColor = System.Drawing.Color.Transparent;
+            this.labelResult.Font = new System.Drawing.Font("Perpetua", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResult.Location = new System.Drawing.Point(197, 309);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 40);
+            this.labelResult.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(770, 545);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.textWidth);
+            this.Controls.Add(this.labelWidth);
+            this.Controls.Add(this.textLength);
+            this.Controls.Add(this.labelLength);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Painting Estimates";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelLength;
+        private System.Windows.Forms.TextBox textLength;
+        private System.Windows.Forms.Label labelWidth;
+        private System.Windows.Forms.TextBox textWidth;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ PaintingEstimates/PaintingEstimates/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaintingEstimates
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textLength.Text, out double length) && double.TryParse(textWidth.Text, out double width))
+            {
+                double totalCost = CalculatePaintingCost(length, width);
+                labelResult.Text = $"Total painting cost: ${totalCost:F2}";
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid numeric values for length and width.");
+            }
+        }
+
+        private double CalculatePaintingCost(double length, double width)
+        {
+            double wallHeight = 9.0;
+            double perimeter = 2 * (length + width);
+            double wallArea = perimeter * wallHeight;
+            double costPerSquareFoot = 6.0;
+            return wallArea * costPerSquareFoot;
+        }
+    }
+}
